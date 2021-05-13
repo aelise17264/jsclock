9146196eaf353249621959a8b504881df3eb4568
unknown <aelise17264@gmail.com>
2021-05-13 15:28:13 -0600

    updated styles on clock

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const hourHand = document.querySelector('.hour')
 
 function setDate(){
     const now = new Date()
-
+    console.log(now)
     // get seconds
     const seconds = now.getSeconds()
     const secondsDegrees = ((seconds/60) * 360) + 90
@@ -14,13 +14,13 @@ function setDate(){
 
     // get minutes
     const mins = now.getMinutes()
-    const minsDegrees = ((mins/60) * 360) +  ((seconds/60)*6) + 90
+    const minsDegrees = ((mins/60) * 360)
 
     minHand.style.transform = `rotate(${minsDegrees}deg)`
 
     // get hours
     const hours = now.getHours()
-    const hoursDegrees = ((hours/12) * 360) + ((mins/60) * 30) + 90
+    const hoursDegrees = ((hours/12) * 360)
 
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
