72379db84fd31f9646232140ffad4a8652caa228
unknown <aelise17264@gmail.com>
2021-05-13 14:30:35 -0600

    added functions to get time for min & hour hands

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const secondHand = document.querySelector('.second')
+const minHand = document.querySelector('.min')
+const hourHand = document.querySelector('.hour')
+
 
 function setDate(){
     const now = new Date()
+
+    // get seconds
     const seconds = now.getSeconds()
-    const secondsDegrees = ((seconds/60) * 360)
+    const secondsDegrees = ((seconds/60) * 360) + 90
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    // get minutes
+    const mins = now.getMinutes()
+    const minsDegrees = ((mins/60) * 360) +  ((seconds/60)*6) + 90
+
+    minHand.style.transform = `rotate(${minsDegrees}deg)`
+
+    // get hours
+    const hours = now.getHours()
+    const hoursDegrees = ((hours/12) * 360) + ((mins/60) * 30) + 90
+
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
 setInterval(setDate, 1000)
